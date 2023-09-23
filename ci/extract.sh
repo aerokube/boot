a8e90e1e85a0f1998ed538c0c46a75dbb8cf0a27
Ivan Krutov <vania-pooh@aerokube.com>
2023-09-23 18:48:06 +0300

    Overwriting extracted file location

--- ci/extract.sh
+++ ci/extract.sh
@@ -5,15 +5,14 @@ VERSION=$1
 
 mkdir -p dist
 cd dist
-for osArch in "linux/amd64" "darwin/amd64" "darwin/arm64" "windows/amd64"; do
+for osArch in "linux_amd64" "darwin_amd64" "darwin_arm64" "windows_amd64"; do
   os=$(echo "$osArch" | awk -F "_" '{print $1;}')
-  arch=$(echo "$osArch" | awk -F "_" '{print $2;}')
   zipFile='kubectl-vm_'"$VERSION"'_'"$osArch.zip"
   downloadURL="https://download.aerokube.com/boot/kubectl-vm/$VERSION/$zipFile"
-  outputFile='kubectl-vm_'"$os"'_'"$arch"
+  outputFile="kubectl-vm_$osArch"
   if [ "$os" == "windows" ]; then
     outputFile="$outputFile.exe"
   fi
-  curl -s "$downloadURL" | zcat >> "$outputFile"
+  curl -s "$downloadURL" | zcat > "$outputFile"
   chmod 755 "$outputFile"
 done
